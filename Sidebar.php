ce4a0c909aacd0d52f0058cd3e29b80960eae310
Manju-Mishra <manju.mishra@neosoftmail.com>
2022-01-27 15:55:05 +0530

    File_handling_Php

---
+++ Sidebar.php
@@ -0,0 +1,10 @@
+<div class="list-group">
+  <a href="?con=image" class="list-group-item list-group-item-action active bg-success">
+    Image
+  </a>
+  <a href="?con=Changeimage" class="list-group-item list-group-item-action">Change Image</a>
+  <a href="?con=Name" class="list-group-item list-group-item-action">Name</a>
+  <a href="?con=Age" class="list-group-item list-group-item-action">Age</a>
+  <a href="?con=gender" class="list-group-item list-group-item-action">Gender</a>
+  
+</div>
